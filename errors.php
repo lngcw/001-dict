42e25c0bf98705560dcf2736e3cdf7326b514025
lngcw <lngcw@mail.ru>
2018-06-30 00:24:27 +0300

    format show code errors

--- errors.php
+++ errors.php
@@ -1,32 +1,32 @@
-<b>1xx Informational</b><br>
-100 Continue
-101 Switching Protocols
-102 Processing
-<b>2xx Success</b><br>
-200 OK
-201 Created
-202 Accepted
-203 Non-Authoritative Information
-204 No content
-205 Reset content
-<b>3xx Redirection</b>
-300 Multiple choices
-301 moved permanently
-302 moved temporarily
-303 found
-304 not modified
-305 use proxy
-<b>4xx client error</b>
-400 bad request
-401 unauthorized
-402 payment required
-403 forbidden
-404 not found
-405 method not allowed
-<b>5xx server error</b>
-500 internal server error
-501 not implemented
-502 bad gateway
-503 service unavailable
-504 gateway timeout
-505 http version not supported
+<b>1xx Informational</b>
+<br>100 Continue
+<br>101 Switching Protocols
+<br>102 Processing
+<br><br><b>2xx Success</b>
+<br>200 OK
+<br>201 Created
+<br>202 Accepted
+<br>203 Non-Authoritative Information
+<br>204 No content
+<br>205 Reset content
+<br><br><b>3xx Redirection</b>
+<br>300 Multiple choices
+<br>301 moved permanently
+<br>302 moved temporarily
+<br>303 found
+<br>304 not modified
+<br>305 use proxy
+<br><br><b>4xx client error</b>
+<br>400 bad request
+<br>401 unauthorized
+<br>402 payment required
+<br>403 forbidden
+<br>404 not found
+<br>405 method not allowed
+<br><br><b>5xx server error</b>
+<br>500 internal server error
+<br>501 not implemented
+<br>502 bad gateway
+<br>503 service unavailable
+<br>504 gateway timeout
+<br>505 http version not supported
--- index.php
+++ index.php
@@ -1,2 +1,2 @@
-<a href="sql/index.php">SQL with PDO</a>
-<a href="errors.php">Error codes</a>
+<a href="sql/index.php">SQL with PDO</a><br>
+<a href="errors.php">Error codes</a><br>
