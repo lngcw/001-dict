a2b02ccde3913b0494f0ddd1eeb6973cc252cc5e
lngcw <lngcw@mail.ru>
2018-06-29 18:38:48 +0300

    Code for 1xx, 2xx, 3xx, 4xx and 5xx errors

---
+++ errors.php
@@ -0,0 +1,32 @@
+<b>1xx Informational</b><br>
+100 Continue
+101 Switching Protocols
+102 Processing
+<b>2xx Success</b><br>
+200 OK
+201 Created
+202 Accepted
+203 Non-Authoritative Information
+204 No content
+205 Reset content
+<b>3xx Redirection</b>
+300 Multiple choices
+301 moved permanently
+302 moved temporarily
+303 found
+304 not modified
+305 use proxy
+<b>4xx client error</b>
+400 bad request
+401 unauthorized
+402 payment required
+403 forbidden
+404 not found
+405 method not allowed
+<b>5xx server error</b>
+500 internal server error
+501 not implemented
+502 bad gateway
+503 service unavailable
+504 gateway timeout
+505 http version not supported
--- index.php
+++ index.php
@@ -1,1 +1,2 @@
 <a href="sql/index.php">SQL with PDO</a>
+<a href="errors.php">Error codes</a>
